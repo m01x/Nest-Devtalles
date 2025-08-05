844557217732d73f8cf4516298b969097b28b680
m01x <fortiz.informatica@gmail.com>
2025-08-05 16:16:54 -0400

    feat(cars): implement update functionality with UpdteCarDto and refactor createCar method in CarsController

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/create-car.dto';
+import { UpdteCarDto } from './dto/update-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -26,13 +27,14 @@ export class CarsController {
     //El DTO formaliza como voy a mover la data por mi app...
     @Post()
     //@UsePipes( ValidationPipe ) <-- Lo migramos a nivel global de aplicacion
-    createCar( @Body() createCar: CreateCarDto){
-        return createCar
+    createCar( @Body() createCarDto: CreateCarDto){
+        return this.carsService.create( createCarDto );
     }
 
     @Patch(':id')
-    updateCar( @Param('id', ParseUUIDPipe) id:string, @Body() body: any){
-        return body
+    updateCar( @Param('id', ParseUUIDPipe) id:string, @Body() updateCarDto: UpdteCarDto){
+
+        return this.carsService.update(id, updateCarDto);
     }
 
     @Delete(':id')
--- 02-car-dealership/src/cars/cars.service.ts
+++ 02-car-dealership/src/cars/cars.service.ts
@@ -1,6 +1,7 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import {v4 as uuidv4} from 'uuid'
 import { Car } from './interfaces';
+import { CreateCarDto, UpdteCarDto } from './dto';
 
 @Injectable()
 export class CarsService {
@@ -37,5 +38,30 @@ export class CarsService {
         if ( !car ) throw new NotFoundException(`Car with id ${id} not found.`);
         
         return car;
+    }
+
+    create( createCarDto: CreateCarDto){
+        const car: Car = {...createCarDto, id: uuidv4()}
+        this.cars.push(car)
+        return this.cars;
+    }
+
+    update( id: string, updateCarDto: UpdteCarDto ){
+
+        let carDB = this.findOnById(id);
+
+        if ( updateCarDto.id && updateCarDto.id !== id) throw new BadRequestException(`Car->ID is not valid inside body`)
+
+        this.cars = this.cars.map( car => {
+            if( car.id === id){
+                carDB = {...carDB, ...updateCarDto, id, }
+                return carDB
+            }
+
+            return car;
+        })
+
+        return carDB //carro actualizado
+
     }
 }
