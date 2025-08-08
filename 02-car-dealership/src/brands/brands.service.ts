42c3e21cb1041d26b924051affac71e6a7280fb8
m01x <fortiz.informatica@gmail.com>
2025-08-08 15:06:30 -0400

    TODO ( brands ): Terminar el CRUD implementado en la sección Brands. ver el service , DTO y Entity (similar al DAO)

--- 02-car-dealership/src/brands/brands.service.ts
+++ 02-car-dealership/src/brands/brands.service.ts
@@ -1,19 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateBrandDto } from './dto/create-brand.dto';
 import { UpdateBrandDto } from './dto/update-brand.dto';
+import { Brand } from './entities/brand.entity';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class BrandsService {
+
+  private brands: Brand[] = [
+    {
+      id: uuidv4(),
+      name: 'Toyota',
+      createdAt: new Date().getTime()
+    }
+  ]
+
   create(createBrandDto: CreateBrandDto) {
-    return 'This action adds a new brand';
+
+    const { name } = createBrandDto;
+
+    const brand : Brand = {
+      id: uuidv4(),
+      name: name.toLowerCase(),
+      createdAt: new Date().getTime()
+    }
+    this.brands.push(brand);
+
+    return brand;
   }
 
   findAll() {
-    return `This action returns all brands`;
+    return this.brands;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} brand`;
+  findOne(id: string) {
+    const brand = this.brands.find( brand => brand.id === id);
+    if ( !brand ) throw new NotFoundException(`Brand with id "${ id }" not found. :P`);
+
+    return brand;
   }
 
   update(id: number, updateBrandDto: UpdateBrandDto) {
