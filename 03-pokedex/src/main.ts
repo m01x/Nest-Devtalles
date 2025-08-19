c9b7c4645fcd91fb71d39b721c37cc994f5598d2
m01x <fortiz.informatica@gmail.com>
2025-08-19 15:13:39 -0400

    Refactor code structure for improved readability and maintainability

--- 03-pokedex/src/main.ts
+++ 03-pokedex/src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v2'); //* Prefijo global, para que la url quede localhost:3000/api/v2/pokemon.. y no tener que especificarlo en cada controller
+
+  //useGlobalPipes , para que class validator y class transformer ejecuten sus validaciones,
+  //aqui se configuran.
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
