f4ca92ed69b27dd52dcd9188dd2e23da9a842ca4
m01x <fortiz.informatica@gmail.com>
2025-08-20 17:59:04 -0400

    feat(pokemon): implement create method with error handling and model integration

--- 03-pokedex/src/pokemon/pokemon.service.ts
+++ 03-pokedex/src/pokemon/pokemon.service.ts
@@ -1,12 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
+
+  constructor(
+    @InjectModel( Pokemon.name )
+    private readonly pokemonModel: Model<Pokemon>
+  ){}
+
+  async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLowerCase();
-    return createPokemonDto;
+
+    try {
+      
+      const pokemon = await this.pokemonModel.create( createPokemonDto );
+      return pokemon;
+
+    } catch (error) {
+      
+      if (error.code === 11000){
+        throw new BadRequestException(`Pokemon already exist in DB. ${ JSON.stringify( error.keyValue ) }` );
+      }
+
+      console.log(error); //El error no es duplicidad de registros, lanzamos otro error.
+      throw new InternalServerErrorException(`Can't create Pokemon - Check server logs`);
+
+    }
+
+
   }
 
   findAll() {
