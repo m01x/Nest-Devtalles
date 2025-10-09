b3b2933b3dbc42e8a15d2de911dec830a70c59a9
m01x <fortiz.informatica@gmail.com>
2025-10-09 17:48:06 -0300

    feat(dtos): enhance pagination and product DTOs with Swagger API properties

--- 04-teslo-shop/src/common/dtos/pagination.dto.ts
+++ 04-teslo-shop/src/common/dtos/pagination.dto.ts
@@ -1,13 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
 import { IsOptional, IsPositive, Min } from "class-validator";
 
 export class PaginationDTO {
 
+    @ApiProperty({
+        default: 10, description:'how many rows do you need'
+    })
     @IsOptional()
     @IsPositive() // Claro, no aparece @IsNumeric o algo asi , porque al especificar qeu sea positivo, va implicito ese decorador.
     @Type( ()=> Number ) //enableImplicitConversions: True
     limit?: number;
 
+    @ApiProperty({
+        default: 0, description:'how many rows do you want to skip'
+    })
     @IsOptional()
     @Type( ()=> Number ) //enableImplicitConversions: True
     @Min(0)
--- 04-teslo-shop/src/products/dto/update-product.dto.ts
+++ 04-teslo-shop/src/products/dto/update-product.dto.ts
@@ -1,4 +1,7 @@
-import { PartialType } from '@nestjs/mapped-types';
+/* 
+*Importacion por defecto. La debemos cambiar por la de Swagger para que tome las @ApiProperty() , definidas en CreateProductDto
+import { PartialType } from '@nestjs/mapped-types'; */
+import { PartialType } from '@nestjs/swagger';
 import { CreateProductDto } from './create-product.dto';
 
 export class UpdateProductDto extends PartialType(CreateProductDto) {}
